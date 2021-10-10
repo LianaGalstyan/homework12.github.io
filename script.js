d100084bb5b2607df51a8d924fd9e7ea8201bc05
Liana <lianagalstyant@gmail.com>
2021-10-10 16:25:29 +0400

    upload files

---
+++ script.js
@@ -0,0 +1,19 @@
+let num1 = 1;
+
+while (num1<=10) {
+    if (num1 % 2 == 0) {
+        alert(num1);
+    }
+    num1++;
+}
+
+
+let num2 = 1;
+while (num2<10) {
+    if (num2 % 2 !== 0) {
+        alert(num2); 
+    }
+    num2++;
+}
+
+
